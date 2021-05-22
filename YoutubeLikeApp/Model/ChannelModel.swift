fae84086051c7cedcc4d669811685bcf1947b9af
hidec-7 <hiedematoaho0730127@gmail.com>
2021-05-22 19:48:16 +0900

    Cellに情報を反映

---
+++ YoutubeLikeApp/Model/ChannelModel.swift
@@ -0,0 +1,23 @@
+//
+//  ChannelModel.swift
+//  YoutubeLikeApp
+//
+//  Created by hideto c. on 2021/05/22.
+//
+
+import Foundation
+
+class ChannelModel: Decodable {
+    
+    let items: [ChannelItem]
+}
+
+class ChannelItem: Decodable {
+    
+    let snippet: ChannelSnippet
+}
+
+class ChannelSnippet: Decodable {
+    
+    let thumbnails: Thumbnail
+}
