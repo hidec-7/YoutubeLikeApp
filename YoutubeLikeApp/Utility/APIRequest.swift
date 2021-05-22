eecb6529fadc41994c1a1ded31d7970781a34594
hidec-7 <hiedematoaho0730127@gmail.com>
2021-05-23 02:38:21 +0900

    Genericsを使ってAPI処理を共通化

---
+++ YoutubeLikeApp/Utility/APIRequest.swift
@@ -0,0 +1,44 @@
+//
+//  APIRequest.swift
+//  YoutubeLikeApp
+//
+//  Created by hideto c. on 2021/05/23.
+//
+
+import Foundation
+import Alamofire
+
+class APIRequest {
+    
+    enum PathType: String {
+        case search
+        case channels
+    }
+    
+    static var shared = APIRequest()
+    
+    private let baseUrl = "https://www.googleapis.com/youtube/v3/"
+    
+    func request<T: Decodable>(path: PathType, params: [String: Any], type: T.Type, completion: @escaping (T) -> Void) {
+        let path = path.rawValue
+        let url = baseUrl + path + "?"
+        
+        var params = params
+        params["key"] = "AIzaSyDpd-0uuuiWpLLn7bUkIJT47JulWyHvt3E"
+        params["part"] = "snippet"
+        
+        let request = AF.request(url, method: .get, parameters: params)
+                                    
+        request.responseJSON { (response) in
+            do {
+                guard let data = response.data else { return }
+                let decode = JSONDecoder()
+                let value = try decode.decode(T.self, from: data)
+                
+                completion(value)
+            } catch {
+                print("変換に失敗しました。: ", error)
+            }
+        }
+    }
+}
