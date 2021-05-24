604792b0ff602bbac93e5df846f09dd0f2990cf4
hidec-7 <hiedematoaho0730127@gmail.com>
2021-05-25 00:47:08 +0900

    TabBarのStoryboard実装

--- YoutubeLikeApp/Controller/BaseTabBarController.swift
+++ YoutubeLikeApp/Controller/BaseTabBarController.swift
@@ -9,24 +9,31 @@ import UIKit
 
 class BaseTabBarController: UITabBarController {
     
+    enum ControllerName: Int {
+        case home, search, plus, channel, library
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupViewControllers()
+    }
+    
+    private func setupViewControllers() {
         viewControllers?.enumerated().forEach({ (index, viewController) in
-            switch index {
-            case 0:
-                setTabBarInfo(viewController, selectedImageName: "home", unSlectedImageName: "home-1", title: "ホーム")
-            case 1:
-                setTabBarInfo(viewController, selectedImageName: "search-bar", unSlectedImageName: "search-bar-1", title: "検索")
-            case 2:
-                setTabBarInfo(viewController, selectedImageName: "plus", unSlectedImageName: "plus-1", title: "")
-            case 3:
-                setTabBarInfo(viewController, selectedImageName: "channel", unSlectedImageName: "channel-1", title: "登録チャンネル")
-            case 4:
-                setTabBarInfo(viewController, selectedImageName: "library", unSlectedImageName: "library-1", title: "ライブラリ")
-            default:
-                break
+            if let name = ControllerName.init(rawValue: index) {
+                switch name {
+                case .home:
+                    setTabBarInfo(viewController, selectedImageName: "home", unSlectedImageName: "home-1", title: "ホーム")
+                case .search:
+                    setTabBarInfo(viewController, selectedImageName: "search-bar", unSlectedImageName: "search-bar-1", title: "検索")
+                case .plus:
+                    setTabBarInfo(viewController, selectedImageName: "plus", unSlectedImageName: "plus-1", title: "")
+                case .channel:
+                    setTabBarInfo(viewController, selectedImageName: "channel", unSlectedImageName: "channel-1", title: "登録チャンネル")
+                case .library:
+                    setTabBarInfo(viewController, selectedImageName: "library", unSlectedImageName: "library-1", title: "ライブラリ")
+                }
             }
         })
     }
