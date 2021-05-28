6ac92eec8fb14ab2b797e272f99879922fe6b946
hidec-7 <hiedematoaho0730127@gmail.com>
2021-05-28 19:48:15 +0900

    ビデオ詳細画面実装

---
+++ YoutubeLikeApp/Controller/VideoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VideoViewController.swift
+//  YoutubeLikeApp
+//
+//  Created by hideto c. on 2021/05/28.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
